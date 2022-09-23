e76c4cb224169e0d78c620a4c5f4185dd4660127
Dave Wang <b08901033@ntu.edu.tw>
2022-09-24 07:50:05 +0800

    the basic version, no data augmentation

--- hw1/hw1_1_basic_train.py
+++ hw1/hw1_1_basic_train.py
@@ -13,27 +13,59 @@ import argparse
 from tqdm.auto import tqdm
 from sklearn.model_selection import train_test_split
 import random
-import module
+# import module
 
 
-trian_path = ""
-test_path = ""
-model_path = ""
-record_path = ""
+class Dataset(data.Dataset):
+	def __init__(self, img_list, transform, random_noise, mode):
+		self.img_list = img_list
+		self.transform = transform
+		self.random_noise = random_noise
+		self.mode = mode
+
+	def __getitem__(self, idx):
+		img_path = self.img_list[idx]
+		original_img = Image.open(img_path)
+		transformed_img = self.transform(original_img)
+		label = 0
+
+		if(self.mode == "test"):
+			label = 0
+		else:
+			label = int(img_path.split("/")[-1].split("_")[0])
+			if(self.mode == "train" and random.random() > 0.5):
+				transformed_img = self.random_noise(transformed_img)
+
+		return transformed_img, label
+
+	def __len__(self):
+		return len(self.img_list)
+
+
+train_path = "./p1_data/train_50"
+# test_path = ""
+model_path = "model.pt"
+record_path = "record.txt"
 
 train_list = glob.glob(os.path.join(train_path, "*.png"))
 train_list, valid_list = train_test_split(train_list, train_size = 0.8, random_state = 2022)
 train_list, valid_list = np.array(train_list), np.array(valid_list)
 
+# train_transform = transforms.Compose([
+# 	transforms.Resize((32, 32)),
+# 	transforms.ToTensor(),
+# 	transforms.Normalize((0.5077, 0.4813, 0.4312), (0.2000, 0.1986, 0.2034)),
+# 	transforms.RandomHorizontalFlip(p = 0.25),
+# 	transforms.RandomInvert(p = 0.25),
+# 	transforms.RandomGrayscale(p = 0.25),
+# 	transforms.RandomAdjustSharpness(10, p = 0.25),
+# 	transforms.ColorJitter(brightness = (0.5, 1.5), contrast = (0.5, 1.5), saturation = (0.5, 1.5), hue = (-0.1, 0.1))
+# ])
+
 train_transform = transforms.Compose([
 	transforms.Resize((32, 32)),
 	transforms.ToTensor(),
 	transforms.Normalize((0.5077, 0.4813, 0.4312), (0.2000, 0.1986, 0.2034)),
-	transforms.RandomHorizontalFlip(p = 0.25),
-	transforms.RandomInvert(p = 0.25),
-	transforms.RandomGrayscale(p = 0.25),
-	transforms.RandomAdjustSharpness(10, p = 0.25),
-	transforms.ColorJitter(brightness = (0.5, 1.5), contrast = (0.5, 1.5), saturation = (0.5, 1.5), hue = (-0.1, 0.1))
 ])
 
 
@@ -46,12 +78,12 @@ valid_transform = transforms.Compose([
 random_noise = transforms.GaussianBlur(kernel_size = (3, 3), sigma = (5, 5))
 
 
-train_imgs = module.Dataset(train_list, transform = train_transform, random_noise = random_noise, mode = "train")
-valid_imgs = module.Dataset(valid_list, transform = valid_transform, random_noise = None, mode = "valid")
+train_imgs = Dataset(train_list, transform = train_transform, random_noise = random_noise, mode = "train")
+valid_imgs = Dataset(valid_list, transform = valid_transform, random_noise = None, mode = "valid")
 
 
-train_loader = DataLoader(dataset = train_imgs, batch_size = 18, shuffle = True, num_workers = 0, pin_memeory = True)
-valid_loader = DataLoader(dataset = valid_imgs, batch_size = 18, shuffle = True, num_workers = 0, pin_memeory = True)
+train_loader = DataLoader(dataset = train_imgs, batch_size = 18, shuffle = True, num_workers = 0, pin_memory = True)
+valid_loader = DataLoader(dataset = valid_imgs, batch_size = 18, shuffle = True, num_workers = 0, pin_memory = True)
 
 
 
@@ -65,7 +97,7 @@ class DenseNet161(nn.Module):
 
 		self.FcLayers = nn.Sequential(
 			nn.Linear(1000, 256),
-			nn.Relu(),
+			nn.ReLU(),
 			nn.Linear(256, 64),
 			nn.ReLU(),
 			nn.Linear(64, 50)
@@ -85,7 +117,7 @@ model.device = device
 prev_epoch = 0
 max_valid_acc = 0
 
-if(os.stat(model_path).st_size == 0):
+if(os.stat(model_path).st_size != 0):
 	checkPoint = None
 	if(torch.cuda.is_available()):
 		checkPoint = torch.load(model_path)
@@ -94,8 +126,8 @@ if(os.stat(model_path).st_size == 0):
 		checkPoint = torch.load(model_path, map_location = "cpu")
 
 	model.load_state_dict(checkPoint["model_state_dict"])
-	prev_epoch = checkpoint["epoch"]
-	max_valid_acc = checkpoint["max_valid_acc"]
+	prev_epoch = checkPoint["epoch"]
+	max_valid_acc = checkPoint["max_valid_acc"]
 
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr = 0.0003, weight_decay = 1e-5)
@@ -121,7 +153,7 @@ for epoch in range(prev_epoch, n_epoch):
 
 		logits = model(imgs.to(device))
 
-		loss = criterion(logits, ladels.to(device))
+		loss = criterion(logits, labels.to(device))
 
 		optimizer.zero_grad()
 
@@ -139,7 +171,7 @@ for epoch in range(prev_epoch, n_epoch):
 	train_loss = sum(train_loss) / len(train_loss)
 	train_acc = sum(train_accs) / len(train_accs)
 
-	print(f"[ Train | {epoch + 1:03d}/{n_epochs:03d} ] loss = {train_loss:.5f}, acc = {train_acc:.5f}")
+	print(f"[ Train | {epoch + 1:03d}/{n_epoch:03d} ] loss = {train_loss:.5f}, acc = {train_acc:.5f}")
 
 	model.eval()
 
@@ -164,29 +196,30 @@ for epoch in range(prev_epoch, n_epoch):
 		valid_accs.append(acc)
 
 	valid_loss = sum(valid_loss) / len(valid_loss)
-    valid_acc = sum(valid_accs) / len(valid_accs)
+	valid_acc = sum(valid_accs) / len(valid_accs)
 
-    print(f"[ Valid | {epoch + 1:03d}/{n_epochs:03d} ] loss = {valid_loss:.5f}, acc = {valid_acc:.5f}")
+	print(f"[ Valid | {epoch + 1:03d}/{n_epoch:03d} ] loss = {valid_loss:.5f}, acc = {valid_acc:.5f}")
 
-    if valid_acc > max_valid_acc:
-        with open(record_path,"a"):
-            print(f"[ Valid | {epoch + 1:03d}/{n_epochs:03d} ] loss = {valid_loss:.5f}, acc = {valid_acc:.5f} -> best")
-    else:
-        with open(record_path,"a"):
-            print(f"[ Valid | {epoch + 1:03d}/{n_epochs:03d} ] loss = {valid_loss:.5f}, acc = {valid_acc:.5f}")
+	if valid_acc > max_valid_acc:
+		with open(record_path,"a") as f:
+			f.write(f"Best model found at epoch {epoch}, saving model\n")
+			f.write(f"[ Valid | {epoch + 1:03d}/{n_epoch:03d} ] loss = {valid_loss:.5f}, acc = {valid_acc:.5f} -> best\n")
+	else:
+		with open(record_path,"a") as f:
+			f.write(f"[ Valid | {epoch + 1:03d}/{n_epoch:03d} ] loss = {valid_loss:.5f}, acc = {valid_acc:.5f}\n")
 
-    if valid_acc > max_valid_acc:
-    	print(f"Best model found at epoch {epoch}, saving model")
-    	torch.save({"model_state_dict" : model.state_dict(),
-    		"epoch" : epoch
-    		"max_valid_acc" : valid_acc
-    	}, model_path)
+	if valid_acc > max_valid_acc:
+		print(f"Best model found at epoch {epoch}, saving model")
+		torch.save({"model_state_dict" : model.state_dict(),
+			"epoch" : epoch,
+			"max_valid_acc" : valid_acc
+		}, model_path)
 
-    	max_valid_acc = valid_acc
-    	stale = 0
-    else:
-    	stale += 1
-    	if stale > patience:
-    		print(f"No improvment {patience} consecutive epochs, early stopping")
-            break
+		max_valid_acc = valid_acc
+		stale = 0
+	else:
+		stale += 1
+		if stale > patience:
+			print(f"No improvment {patience} consecutive epochs, early stopping")
+			break
 
