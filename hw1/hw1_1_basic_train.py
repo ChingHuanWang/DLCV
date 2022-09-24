79ca9f83e8bf7006f0c5e2acdc84e87c8bfc7725
Dave Wang <b08901033@ntu.edu.tw>
2022-09-24 09:20:55 +0800

    merge current work version

--- hw1/hw1_1_basic_train.py
+++ hw1/hw1_1_basic_train.py
@@ -16,6 +16,17 @@ import random
 # import module
 
 
+# set a random seed for reproducibility
+myseed = 6666  
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(myseed)
+torch.manual_seed(myseed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(myseed)
+
+
+
 class Dataset(data.Dataset):
 	def __init__(self, img_list, label_list, transform, random_noise, mode):
 		self.img_list = img_list
